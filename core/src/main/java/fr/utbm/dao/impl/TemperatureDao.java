2b0ad443cf1955a982b8cf9cacbf59cc0deb9720
Callan57 <julien.kempf.57@gmail.com>
2014-11-12 11:50:17 +0100

    Le nouveau core

--- core/src/main/java/fr/utbm/dao/impl/TemperatureDao.java
+++ core/src/main/java/fr/utbm/dao/impl/TemperatureDao.java
@@ -1,31 +1,20 @@
 package fr.utbm.dao.impl;// default package
 // Generated 31 oct. 2014 13:51:02 by Hibernate Tools 3.4.0.CR1
 
-import java.util.List;
-
+import fr.utbm.core.entity.Temperature;
+import fr.utbm.dao.HibernateDao;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import fr.utbm.core.entity.Temperature;
-import fr.utbm.dao.HibernateDao;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.criterion.Projections;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
 /**
  * Home object for domain model class Temperature.
- * @see .Temperature
+ *
  * @author Hibernate Tools
+ * @see .Temperature
  */
-@Repository
-public class TemperatureDao extends HibernateDao implements ITemperatureDao {
-
-    private static Session session;
-
-    static {
-        session = getSession();
-    }
+public class TemperatureDao extends HibernateDao {
 
 	private static final Log log = LogFactory.getLog(TemperatureDao.class);
 
@@ -44,7 +33,7 @@ public class TemperatureDao extends HibernateDao implements ITemperatureDao {
 		super.delete(persistentInstance);
 	}
 
-	public Temperature findById(java.lang.String id) {
+	public Temperature findById(int id) {
 		log.debug("getting Temperature instance with id: " + id);
 		return (Temperature) super.get(Temperature.class, id);
 	}
@@ -54,19 +43,4 @@ public class TemperatureDao extends HibernateDao implements ITemperatureDao {
 		log.debug("getting all Temperature instances");
 		return (List<Temperature>) super.getAll(Temperature.class);
 	}
-
-    public List<Temperature> getLastByStation()
-    {
-        log.debug("getting last Temperature for all Station");
-        begin();
-        Query query = session.createQuery(
-            "from Temperature as te " +
-                "inner join te.sensor as se " +
-                "inner join se.station as st " +
-                "group by st.staId"
-
-        );
-        commit();
-        return query.list();
-    }
 }
